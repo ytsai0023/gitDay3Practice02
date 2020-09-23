4d83c151bf986806da2c8cacbd4be96027e4ba28
ysai.pro@gmail.com <ysai.pro@gmail.com>
2020-09-24 07:50:53 +0900

    JS 功能二

--- all.js
+++ all.js
@@ -7,3 +7,21 @@ function fn1() {
     }
   }
 }
+
+//功能二
+//BMI = 體重(公斤) / 身高2(公尺2)
+function calcBMI(h = 0, w = 0) {
+  let h_m = h / 100;
+  let BMI = (w / Math.pow(h_m, 2)).toFixed(1);
+  console.log(BMI);
+  return BMI;
+}
+
+document.getElementById('calcBMI').addEventListener('click', (e) => {
+  e.preventDefault();
+  const height = parseFloat(document.getElementById('height').value);
+  const weight = parseFloat(document.getElementById('weight').value);
+  if (!(height || weight)) return;
+  const _BMI = calcBMI(height, weight);
+  document.getElementById('result').innerText = _BMI;
+});
